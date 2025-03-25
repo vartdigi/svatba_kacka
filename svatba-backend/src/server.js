d8d2eefc9b5fd0729b0f7027e4e8365b74095c43
Vardui Saribekian <vartvioline@gmail.com>
2025-03-25 19:59:47 +0100

    nevim

--- svatba-backend/src/server.js
+++ svatba-backend/src/server.js
@@ -3,6 +3,15 @@ require('dotenv').config(); // Načte proměnné z .env souboru
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const mongoUri = process.env.MONGO_URI;
+
+mongoose
+  .connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('Error connecting to MongoDB', err));
 
 const app = express();
 
@@ -15,14 +24,6 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 app.use(express.json());
-
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => console.error('Error connecting to MongoDB', err));
 
 const voteSchema = new mongoose.Schema({
   prijezd: {
